613a213cf2d2f6adf4f63e149a1ca5bd99f5a0ef
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2023-11-30 09:02:48 -0300

    feature/Adds initial structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+
+export function App() {
+
+  return (
+    <>
+      <Post 
+        author="Mano Deivin" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit pariatur esse necessitatibus accusantium! Aliquam atque eum repudiandae numquam neque sed eveniet corrupti, porro ea dolores. Nulla repellat neque eius sapiente." 
+      />
+      <Post author="Fulano" content="Só queria postar" />       
+    </>
+  )
+}
+
+
