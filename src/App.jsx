7daa8fa0810e203e47a754b4812d92d647d13215
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2023-12-01 08:37:20 -0300

    feature/Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header/Header";
+import './global.css';
 
 export function App() {
 
   return (
     <>
+    <Header />
       <Post 
         author="Mano Deivin" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit pariatur esse necessitatibus accusantium! Aliquam atque eum repudiandae numquam neque sed eveniet corrupti, porro ea dolores. Nulla repellat neque eius sapiente." 
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 export function Post(props) {    
     return (
         <>
