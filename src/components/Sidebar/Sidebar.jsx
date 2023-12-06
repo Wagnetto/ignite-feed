a40b88657487d76b60e619f9ea5a3776cccd1151
Wagner Silveira Souto <wagnerssouto@gmail.com>
2023-12-06 11:38:05 -0300

    feature/Profile sidebar styles

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine} from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -8,11 +9,15 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1701111098056-9de8cb79bf7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2NHx8fGVufDB8fHx8fA%3D%3D" 
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://github.com/Wagnetto.png' />
                 <strong>Wagnetto</strong>
                 <span>Front-end developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>                    
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
